47f76657d59ef101e72565d0f5bed8abc07fc83e
Jonan <84633272+SJunwikul@users.noreply.github.com>
2024-07-24 10:59:20 -0700

    -

--- auth/tests/setup.js
+++ auth/tests/setup.js
@@ -1,6 +1,6 @@
-const DebugControl = require('../utilities/debug.js')
-DebugControl.setLevel(DebugControl.levels.NONE)
+const DebugControl = require("../utilities/debug.js");
+DebugControl.setLevel(DebugControl.levels.ALL);
 
 module.exports = {
-  server: require('../app.js')
-}
+  server: require("../app.js"),
+};
